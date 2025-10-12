cc05d7d2d3476302300af8221fa135c2167c117b
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-10-12 22:12:30 +0700

    add picocli in dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.5")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
