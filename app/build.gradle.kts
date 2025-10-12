5287c4f140c66496387fc2aa335b99354ed3cf2d
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-10-12 21:08:41 +0700

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     id("java")
+    id("com.github.ben-manes.versions") version "0.52.0"
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("io.hexlet.Application") }
 
 repositories {
     mavenCentral()
