82b0a4e0f8b654d1de671baaa5c385d5aee27dbc
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-10-12 22:24:16 +0700

    rename and create help util

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0"
+)
+public class App {
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+}
