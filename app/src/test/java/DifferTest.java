07b6dcfa5372e192098865525b9e43a67bc4e349
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-10-17 21:26:01 +0700

    create differ test

---
+++ app/src/test/java/DifferTest.java
@@ -0,0 +1,22 @@
+import hexlet.code.Differ;
+import org.junit.jupiter.api.Test;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DifferTest {
+    @Test
+    public void testGenerateFlatJson() throws Exception {
+        String file1 = Path.of("src", "test", "resources", "file1.json").toString();
+        String file2 = Path.of("src", "test", "resources", "file2.json").toString();
+
+        String actual = Differ.generate(file1, file2);
+
+        assertTrue(actual.contains("- follow: false"));
+        assertTrue(actual.contains("host: hexlet.io"));
+        assertTrue(actual.contains("- proxy: 123.234.53.22"));
+        assertTrue(actual.contains("- timeout: 50"));
+        assertTrue(actual.contains("+ timeout: 20"));
+        assertTrue(actual.contains("+ verbose: true"));
+    }
+}
